166386b7d4d2b48df9f670d6587dc6fc29bbe15d
Vishnu Prabhu <VishnuPrabhu.Ramachandran@uobgroup.com>
2020-05-07 20:44:52 +0800

    first commit of Repeat-IOS tutorials

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by veneq7 on 7/5/20.
+//  Copyright © 2020 veneq7. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
